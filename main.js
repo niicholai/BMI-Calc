3b7ebc5e6ce5f8128c13aba79f3a2b1e2b50a591
Derek F <derek.folds@gmail.com>
2021-12-23 01:06:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,34 @@ function bmiPromptImperial() {
   var heightImp = prompt("What is your height in inches?");
   var weightImp = prompt("What is your weight in pounds?");
   var finalBmiImp = ((weightImp / (heightImp * heightImp)) * 703);
-  alert("Your BMI is " + Math.floor(finalBmiImp) + ", hope this has been informative!")
+  if (finalBmiImp < 18.5) {
+    alert("Your BMI is " + round(finalBmiImp, 1) + ", this indicates you are underweight.");
+  }
+  if (finalBmiImp >= 18.5 && finalBmiImp <= 24.9) {
+    alert("Your BMI is " + round(finalBmiImp, 1) + ", this indicates you are of normal weight.");
+  }
+  if (finalBmiImp >= 25 && finalBmiImp <=29.9) {
+    alert("Your BMI is " + round(finalBmiImp, 1) + ", this indicates you are overweight.");
+  }
+  if (finalBmiImp >= 30) {
+    alert("Your BMI is " + round(finalBmiImp, 1) + ", this indicates you are obese.");
+  }
 }
 
 function bmiPromptMetric() {
   var heightMet = prompt("What is your height in centimeters?");
   var weightMet = prompt("What is your weight in kilograms?");
   var finalBmiMet = ((weightMet / (heightMet * heightMet)) * 10000);
-  alert("Your BMI is " + Math.floor(finalBmiMet) + ", hope this has been informative!")
+  if (finalBmiMet < 18.5) {
+    alert("Your BMI is " + round(finalBmiMet, 1) + ", this indicates you are underweight.");
+  }
+  if (finalBmiMet >= 18.5 && finalBmiMet <= 24.9) {
+    alert("Your BMI is " + round(finalBmiMet, 1) + ", this indicates you are of normal weight.");
+  }
+  if (finalBmiMet >= 25 && finalBmiMet <=29.9) {
+    alert("Your BMI is " + round(finalBmiMet, 1) + ", this indicates you are overweight.");
+  }
+  if (finalBmiMet >= 30) {
+    alert("Your BMI is " + round(finalBmiMet, 1) + ", this indicates you are obese.");
+  }
 }
