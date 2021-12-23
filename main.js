419f0ef3f61b78d284a069e85855b243be70f389
Derek F <derek.folds@gmail.com>
2021-12-23 02:04:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ function bmiPromptMetric() {
   var weightMet = prompt("What is your weight in kilograms?");
   var finalBmiMet = ((weightMet / (heightMet * heightMet)) * 10000);
   if (finalBmiMet < 18.5) {
-    alert("Your BMI is " + finalBmiImp.toFixed(1) + ", this indicates you are underweight.");
+    alert("Your BMI is " + finalBmiMet.toFixed(1) + ", this indicates you are underweight.");
   }
   if (finalBmiMet >= 18.5 && finalBmiMet <= 24.9) {
-    alert("Your BMI is " + finalBmiImp.toFixed(1) + ", this indicates you are of normal weight.");
+    alert("Your BMI is " + finalBmiMet.toFixed(1) + ", this indicates you are of normal weight.");
   }
   if (finalBmiMet >= 25) {
-    alert("Your BMI is " + finalBmiImp.toFixed(1) + ", this indicates you are overweight.");
+    alert("Your BMI is " + finalBmiMet.toFixed(1) + ", this indicates you are overweight.");
   }
 };
