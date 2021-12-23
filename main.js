a91092cc8ef681a68c8c2d96e1706cf07b129f44
Derek F <derek.folds@gmail.com>
2021-12-23 01:57:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,16 +3,13 @@ function bmiPromptImperial() {
   var weightImp = prompt("What is your weight in pounds?");
   var finalBmiImp = ((weightImp / (heightImp * heightImp)) * 703);
   if (finalBmiImp < 18.5) {
-    alert("Your BMI is " + round(finalBmiImp, 1) + ", this indicates you are underweight.");
+    alert("Your BMI is " + finalBmiImp.toFixed(1) + ", this indicates you are underweight.");
   }
   if (finalBmiImp >= 18.5 && finalBmiImp <= 24.9) {
-    alert("Your BMI is " + round(finalBmiImp, 1) + ", this indicates you are of normal weight.");
+    alert("Your BMI is " + finalBmiImp.toFixed(1) + ", this indicates you are of normal weight.");
   }
-  if (finalBmiImp >= 25 && finalBmiImp <=29.9) {
-    alert("Your BMI is " + round(finalBmiImp, 1) + ", this indicates you are overweight.");
-  }
-  if (finalBmiImp >= 30) {
-    alert("Your BMI is " + round(finalBmiImp, 1) + ", this indicates you are obese.");
+  if (finalBmiImp >= 25) {
+    alert("Your BMI is " + finalBmiImp.toFixed(1) + ", this indicates you are overweight.");
   }
 };
 
@@ -21,15 +18,12 @@ function bmiPromptMetric() {
   var weightMet = prompt("What is your weight in kilograms?");
   var finalBmiMet = ((weightMet / (heightMet * heightMet)) * 10000);
   if (finalBmiMet < 18.5) {
-    alert("Your BMI is " + round(finalBmiMet, 1) + ", this indicates you are underweight.");
+    alert("Your BMI is " + finalBmiImp.toFixed(1) + ", this indicates you are underweight.");
   }
   if (finalBmiMet >= 18.5 && finalBmiMet <= 24.9) {
-    alert("Your BMI is " + round(finalBmiMet, 1) + ", this indicates you are of normal weight.");
+    alert("Your BMI is " + finalBmiImp.toFixed(1) + ", this indicates you are of normal weight.");
   }
-  if (finalBmiMet >= 25 && finalBmiMet <=29.9) {
-    alert("Your BMI is " + round(finalBmiMet, 1) + ", this indicates you are overweight.");
-  }
-  if (finalBmiMet >= 30) {
-    alert("Your BMI is " + round(finalBmiMet, 1) + ", this indicates you are obese.");
+  if (finalBmiMet >= 25) {
+    alert("Your BMI is " + finalBmiImp.toFixed(1) + ", this indicates you are overweight.");
   }
 };
